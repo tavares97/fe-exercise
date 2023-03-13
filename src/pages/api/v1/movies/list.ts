e035fd340fda49edd11c3abef2fe9be68c2f5e3a
tavares97 <afpt.digital2013@gmail.com>
2023-03-13 19:12:31 +0000

    Created/tested  API endpoint, added tailwind

--- src/pages/api/v1/movies/list.ts
+++ src/pages/api/v1/movies/list.ts
@@ -1,10 +1,19 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import data from "./data.json";
 
-const IMAGES_BASE_PATH = "https://image.tmdb.org/t/p/w500/"
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    /**Taking the data from the data.json file and mapping it to the movies variable,
+     * adds the correct path to the images and returns it
+     *  */
+    const movies = data.results.map((movie) => ({
+      ...movie,
+      poster_path: `${process.env.IMAGES_BASE_PATH}${movie.poster_path}`,
+    }));
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-    res.status(200).json({ status: 'OK' })
+    res.status(200).json(movies);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error });
+  }
 }
